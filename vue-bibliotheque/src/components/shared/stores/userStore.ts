0049162ff85dcb7246f2a5571d4558722f7176f1
Jallal <jallaldevweb@gmail.com>
2025-09-23 10:43:31 +0200

    Add Pinia Store and Userinterface

---
+++ vue-bibliotheque/src/components/shared/stores/userStore.ts
@@ -0,0 +1,14 @@
+import { defineStore } from "pinia";
+import type { User } from "../interfaces";
+
+interface UserState {
+    currentUser: User | null
+}
+
+export const useUser = defineStore('user', 
+    {
+        state: (): UserState => ({
+            currentUser:null
+        })
+    }
+)
